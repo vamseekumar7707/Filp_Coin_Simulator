69cabbf9b37480f96097d7118fe41c189be91f1d
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-10 01:47:27 +0530

    Added : Constraint that either of them wins 21 times

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -x
 #FLIP COIN SIMULATOR
+
 countHead=0
 countTail=0
-for (( i=0; i<100; i++ ))
+while [ $countHead -ne 21 ] || [ $countTail -ne 21 ]
 do
 toss=$((RANDOM%2))
 
         if [ $toss -eq 0 ]
+then
+
+        ((countHead++))
+if [ $countHead -eq 21 ]
         then
-         ((countHead++))
-        else
-        ((countTail++))
-
-        fi
+        echo "Head Wins by : " $(($countHead-$countTail))
+        break
+fi
+else
+      ((countTail++))
+if [ $countTail -eq 21 ]
+        then
+        echo "Tail Wins by :" $(($countTail-$countHead))
+        break
+fi
+fi
 done
-
-           echo "Head wins " $countHead
-
-           echo "Tail wins " $countTail
-
