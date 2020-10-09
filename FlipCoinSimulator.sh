65e16cf7656b177699c703600a6d462e64ee3bfd
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-10 01:55:28 +0530

    Added: IF tie occur , continue minimum of 2 points is achieved

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,27 +1,71 @@
 #!/bin/bash -x
 #FLIP COIN SIMULATOR
-
 countHead=0
 countTail=0
 while [ $countHead -ne 21 ] || [ $countTail -ne 21 ]
-do
-toss=$((RANDOM%2))
+   do
+      toss=$((RANDOM%2))
 
         if [ $toss -eq 0 ]
-then
+	then
 
         ((countHead++))
 if [ $countHead -eq 21 ]
         then
-        echo "Head Wins by : " $(($countHead-$countTail))
+        Headdiff=$(($countHead-$countTail))
+        echo "Head Wins by : " $Headdiff
+
         break
 fi
-else
-      ((countTail++))
-if [ $countTail -eq 21 ]
+        else
+        ((countTail++))
+        if [ $countTail -eq 21 ]
         then
-        echo "Tail Wins by :" $(($countTail-$countHead))
+        Taildiff=$(($countTail-$countHead))
+
+        echo "Tail Wins by :" $Taildiff
         break
+
 fi
 fi
 done
+
+echo $Headdiff
+echo $Taildiff
+
+#Tie situation
+
+if [ $Headdiff -eq 0 ] || [ $Taildiff -eq 0 ]
+  then
+
+while [ $Headdiff -gt 2 ] || [ $Taildiff -gt 2 ]
+
+  do
+  toss=$((RANDOM%2))
+
+        if [ $toss -eq 0 ]
+         then
+
+        ((countHead++))
+if [ $countHead -gt 2 ]
+        then
+        Headdiff=$(($countHead-$countTail))
+
+        echo "First time Head/Tail is same, Now Head Wins by : " $Headdiff
+        break
+fi
+        else
+        ((countTail++))
+        if [ $countTail -gt 2 ]
+        then
+        Taildiff= $(($countTail-$countHead))
+echo "First time Head/Tail is same, Now Tail Wins by :" $Taildiff
+        break
+
+fi
+fi
+done
+
+
+fi
+
