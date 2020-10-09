e6e655608ac906be9a1324b61805d9a17e23fc0e
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-10 01:26:18 +0530

    Added:Head or Tails wins

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 #FLIP COIN SIMULATOR
+ countHead=0
+ countTail=0
+toss=$((RANDOM%2))
+        if [ $toss -eq 0 ]
+        then
+        echo "Head wins"
+        else
+        echo "Tail wins"
+	fi
